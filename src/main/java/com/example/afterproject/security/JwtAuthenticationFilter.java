20907dabc49fa75aa5a68bc0169043c85cf8fada
JungTaeMin <jungtaemin@JungTaeMinui-MacBookAir.local>
2025-12-19 18:21:12 +0900

    3313dda

--- src/main/java/com/example/afterproject/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/afterproject/security/JwtAuthenticationFilter.java
@@ -19,10 +19,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = jwtTokenProvider.resolveToken(request);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception e) {
+            // 토큰은 유효한 형식이나, 사용자를 찾을 수 없는 경우(DB 초기화 등)
+            // 에러를 발생시키지 않고 SecurityContext를 비워서 '익명 사용자'로 처리
+            SecurityContextHolder.clearContext();
+
+            // 필요하다면 로그 출력
+            // logger.error("Authentication Failed: " + e.getMessage());
         }
+
         filterChain.doFilter(request, response);
     }
 }
